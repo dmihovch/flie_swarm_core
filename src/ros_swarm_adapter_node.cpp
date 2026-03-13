cb68acc414a47e630797c1791618f95a8cf95d2f
Daniel Mihovch <dmihovch@gmail.com>
2026-03-13 10:41:10 -0400

    holy shirt it flies

--- src/ros_swarm_adapter_node.cpp
+++ src/ros_swarm_adapter_node.cpp
@@ -1,41 +1,39 @@
 #include <ros/ros.h>
-#include <geometry_msgs/TwistStamped.h>
+#include <geometry_msgs/Twist.h>
 #include <nav_msgs/Odometry.h>
 
-geometry_msgs::TwistStamped current_cmd;
+ros::Publisher cmd_pub;
+geometry_msgs::Twist current_cmd;
+bool odom_init = false;
+double start_alt = 0.0;
 
 void odometryCallback(const nav_msgs::Odometry::ConstPtr& msg) {
-    double z_pos = msg->pose.pose.position.z;
-    
-    if (z_pos < 2.0) {
-        current_cmd.twist.linear.z = 0.5;
-        current_cmd.twist.linear.x = 0.0;
-		current_cmd.twist.linear.y = 0.0;
-    } else {
-        current_cmd.twist.linear.z = 0.0;
-        current_cmd.twist.linear.x = 0.2;
-		current_cmd.twist.linear.y = 0.0;
+    if (!odom_init) {
+        start_alt = msg->pose.pose.position.z;
+        odom_init = true;
     }
 
-    ROS_INFO_THROTTLE(1.0, "Altitude: %.2f m | Cmd Z: %.1f | Cmd X: %.1f", z_pos, current_cmd.twist.linear.z, current_cmd.twist.linear.x);
-}
+    double z_pos = msg->pose.pose.position.z;
 
-void controlLoopCallback(const ros::TimerEvent&, ros::Publisher& pub) {
-    current_cmd.header.stamp = ros::Time::now();
-    current_cmd.header.frame_id = "base_stabilized";
-    pub.publish(current_cmd);
+    if (z_pos < start_alt + 2.0) {
+        current_cmd.linear.z = 0.5;
+        current_cmd.linear.x = 0.0;
+    } else {
+        current_cmd.linear.z = 0.0;
+        current_cmd.linear.x = 0.5;
+    }
+
+    cmd_pub.publish(current_cmd);
+    ROS_INFO_THROTTLE(1.0, "Alt: %.2f m | Cmd Z: %.1f | Cmd X: %.1f", z_pos, current_cmd.linear.z, current_cmd.linear.x);
 }
 
 int main(int argc, char** argv) {
     ros::init(argc, argv, "ros_swarm_adapter_node");
     ros::NodeHandle nh;
 
-    ros::Subscriber odom_sub = nh.subscribe("/swarm_member_1/ground_truth/odometry", 10, odometryCallback);
-    ros::Publisher cmd_pub = nh.advertise<geometry_msgs::TwistStamped>("/swarm_member_1/command/twist", 10);
-
-    ros::Timer timer = nh.createTimer(ros::Duration(0.02), boost::bind(controlLoopCallback, _1, boost::ref(cmd_pub)));
+    cmd_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel", 10);
+    ros::Subscriber odom_sub = nh.subscribe("ground_truth/state", 10, odometryCallback);
 
     ros::spin();
-
     return 0;
 }
