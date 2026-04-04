f53dbf8ad6fc849b12657e8134cc5db1275b0eca
Daniel Mihovch <dmihovch@gmail.com>
2026-04-04 16:10:09 -0400

    bruh

--- src/ros_swarm_adapter_node.cpp
+++ src/ros_swarm_adapter_node.cpp
@@ -8,7 +8,10 @@ int main(int argc, char** argv) {
     ros::NodeHandle nh_private("~");
 
     double x_dir = 1.0;
+    double y_mult = 1.0;
+    
     nh_private.param("x_dir",x_dir,1.0);
+    nh_private.param("y_mult",y_mult, 1.0);
 
     ros::Publisher dji_control_pub = nh.advertise<sensor_msgs::Joy>("dji_sdk/flight_control_setpoint_ENUvelocity_yawrate",10);
     ros::Rate loop_rate(50);
@@ -27,7 +30,7 @@ int main(int argc, char** argv) {
         double t = (ros::Time::now() - start_time).toSec();
 
         control_msg.axes[0] = x_dir * 0.5;
-        control_msg.axes[1] = sin(t);
+        control_msg.axes[1] = y_mult * 0.2 * sin(t * 0.5);
 
         if(t < 30.0)
         {
