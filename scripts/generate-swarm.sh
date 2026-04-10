1dc5429c6b695c5418700886052871314a74bb8a
Daniel Mihovch <dmihovch@gmail.com>
2026-04-09 20:13:43 -0400

    final commit before VIP presentation

--- scripts/generate-swarm.sh
+++ scripts/generate-swarm.sh
@@ -13,31 +13,35 @@ cat << EOF > $OUT_FILE
 EOF
 
 for i in $(seq 1 $SWARM_SIZE); do
-  if [ "$FORMATION" == "v_shape" ]; then
-      if [ $i -eq 1 ]; then
-        X_OFFSET=0
-        Y_OFFSET=0
-      else
-        RANK=$(( i / 2 ))
-        IS_LEFT=$(( i % 2 ))
-        X_OFFSET=$(( RANK * -2 ))
-        if [ $IS_LEFT -eq 0 ]; then
-          Y_OFFSET=$(( RANK * 2 ))
-        else
-          Y_OFFSET=$(( RANK * -2 ))
-        fi
-      fi
-    else
+  if [ "$FORMATION" == "flying_v" ]; then
+    if [ $i -eq 1 ]; then
       X_OFFSET=0
-      Y_OFFSET=$(( (i - 1) * 2 ))
+      Y_OFFSET=0
+    else
+      RANK=$(( i / 2 ))
+      IS_LEFT=$(( i % 2 ))
+      X_OFFSET=$(( RANK * -2 ))
+      if [ $IS_LEFT -eq 0 ]; then
+        Y_OFFSET=$(( RANK * 2 ))
+      else
+        Y_OFFSET=$(( RANK * -2 ))
+      fi
     fi
+  elif [ "$FORMATION" == "helix" ] || [ "$FORMATION" == "carousel" ]; then
+    PHASE=$(LC_ALL=C awk -v i="$i" -v size="$SWARM_SIZE" 'BEGIN {print (2.0 * 3.14159 * (i - 1)) / size}')
+    X_OFFSET=$(LC_ALL=C awk -v phase="$PHASE" 'BEGIN {printf "%.2f", 3.0 * cos(phase)}')
+    Y_OFFSET=$(LC_ALL=C awk -v phase="$PHASE" 'BEGIN {printf "%.2f", 3.0 * sin(phase)}')  
+  else
+    X_OFFSET=0
+    Y_OFFSET=$(( (i - 1) * 2 ))
+  fi
 
-    cat << EOF >> $OUT_FILE
+  cat << EOF >> $OUT_FILE
 
   <include file="\$(find flie_swarm_core)/launch/spawn_drone.launch">
     <arg name="drone_name" value="drone_${i}"/>
-    <arg name="x" value="${X_OFFSET}.0"/>
-    <arg name="y" value="${Y_OFFSET}.0"/>
+    <arg name="x" value="${X_OFFSET}"/>
+    <arg name="y" value="${Y_OFFSET}"/>
     <arg name="drone_id" value="${i}"/>
     <arg name="swarm_size" value="${SWARM_SIZE}"/>
     <arg name="swarm_formation" value="${FORMATION}"/>
@@ -46,4 +50,4 @@ EOF
 done
 
 echo "</launch>" >> $OUT_FILE
-echo "Generated $OUT_FILE for $SWARM_SIZE drones."
+echo "Generated $OUT_FILE for $SWARM_SIZE drones in $FORMATION formation."
--- src/ros_swarm_adapter_node.cpp
+++ src/ros_swarm_adapter_node.cpp
@@ -4,6 +4,12 @@
 #include <algorithm>
 #include <string>
 
+#define IDLE_TIME 5.0
+#define UPWARD_THRUST_TIME IDLE_TIME + 3.0
+#define SLOW_HOVER_TIME UPWARD_THRUST_TIME + 3.0
+#define T_RESET SLOW_HOVER_TIME
+
+
 int main(int argc, char** argv) {
     ros::init(argc, argv, "ros_swarm_adapter_node");
     ros::NodeHandle nh;
@@ -34,45 +40,55 @@ int main(int argc, char** argv) {
         control_msg.axes.resize(4);
         double t = (ros::Time::now() - start_time).toSec();
         
-        if (t < 5.0) {
-            control_msg.axes[0] = 0.0;
-            control_msg.axes[1] = 0.0;
-            control_msg.axes[2] = 2.0;
-        } else if (t < 8.0) {
+        if (t < IDLE_TIME) {
             control_msg.axes[0] = 0.0;
             control_msg.axes[1] = 0.0;
             control_msg.axes[2] = 0.0;
-        } else {
+        } else if (t < UPWARD_THRUST_TIME) {
+            control_msg.axes[0] = 0.0;
+            control_msg.axes[1] = 0.0;
+            if(swarm_formation == "helix")
+            {
+                control_msg.axes[2] = 1.0 + ((drone_id - 1)*0.3);
+            }
+            else{
+                control_msg.axes[2] = 2.0;
+            }
+        } else if(t < SLOW_HOVER_TIME) {
+            control_msg.axes[0] = 0.0;
+            control_msg.axes[1] = 0.0;
+            control_msg.axes[2] = 0.2;
+        }
+        else {
 
             
-            double swarm_time = t - 8.0;
-            double blend = std::min(1.0,swarm_time/3.0);
+            double swarm_time = t - T_RESET;
 
-            if(swarm_formation == "flying_v")
-            {
-                double forward_speed = 1.0;
+            if(swarm_formation == "flying_v") {
+                double forward_speed = 3.0;
                 control_msg.axes[0] = forward_speed;
                 control_msg.axes[1] = 0.0;
                 control_msg.axes[2] = 0.0;
             }
-            else if(swarm_formation == "helix")
-            {
-                double omega = 1.0;
-                double radius = 2.0;
-                control_msg.axes[0] = blend * -radius * omega * sin(omega * swarm_time + phase_offset);
-                control_msg.axes[1] = blend * radius * omega * cos(omega * swarm_time + phase_offset);
-                control_msg.axes[2] = blend * 0.2;
+            else if(swarm_formation == "helix") {
+
+                double phase_offset = (2.0 * M_PI * (drone_id - 1)) / swarm_size;
+                double omega = 0.5;
+                double radius = 3.0; 
+                control_msg.axes[0] = -radius * omega * sin(omega * swarm_time + phase_offset);
+                control_msg.axes[1] = radius * omega * cos(omega * swarm_time + phase_offset);
+                control_msg.axes[2] = 0;
             }
-            else if(swarm_formation == "carousel")
-            {
+            else if(swarm_formation == "carousel") {
+                double phase_offset = (2.0 * M_PI * (drone_id - 1)) / swarm_size;
                 double omega = 0.5;
                 double radius = 3.0;
                 double z_amplitude = 0.5;
-                control_msg.axes[0] = blend * -radius * omega * sin(omega * swarm_time + phase_offset);
-                control_msg.axes[1] = blend * radius * omega * cos(omega * swarm_time + phase_offset);
-                control_msg.axes[2] = blend * z_amplitude * cos(2.0 * omega * swarm_time + phase_offset);
+                control_msg.axes[0] = -radius * omega * sin(omega * swarm_time + phase_offset);
+                control_msg.axes[1] = radius * omega * cos(omega * swarm_time + phase_offset);
+                control_msg.axes[2] = z_amplitude * cos(3.0 * omega * swarm_time + phase_offset);
             }
-        }
+       }
 
         control_msg.axes[3] = 0.0;
 
